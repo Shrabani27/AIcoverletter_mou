e8193eb7ee554bd176ef50f0382ea59e88543dd0
Shrabani27 <shrabanighoshmou@gmail.com>
2025-11-27 14:54:52 +0530

    added streamlit codes for AI cover letter

---
+++ AIcoverletter.py
@@ -0,0 +1,18 @@
+import google.generativeai as genai
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+st.title("AI Cover Letter Generator")
+job_title = st.text_input("Enter Job Title")
+resume = st.text_area("Paste your resume summary here")
+
+if st.button("Generate cover letter"):
+    prompt = f"""
+    write a professional cover letter for the job title "{job_title}".
+    use this resume summary:
+    {resume}
+    """
+
+    output = ask_gemini(prompt)
+    st.subheader("Generated cover letter")
+    st.write(output)
